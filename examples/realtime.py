0a72e9c5c4f6e07d2a40785670dc4fe52c4dccc7
Alejandra Duran <ad2002@princeton.edu>
2024-12-02 18:53:11 -0500

    first sturcture

--- examples/realtime.py
+++ examples/realtime.py
@@ -1,7 +1,7 @@
 """Use Blazepose to detect 33 keypoints on image frames coming from real-time
 video stream using opencv"""
 
-# FROM LATER
+# I don't see the difference between blaze pose and normal pose from mediapipe
 
 import cv2
 import mediapipe as mp
@@ -29,6 +29,9 @@ while True:
 
     # Process the frame with BlazePose
     results = pose.process(frame_rgb)
+    
+    ## MODEL COME IN - process the keypoints detected and classify the pose
+    ## and display it
 
     # Draw landmarks if detected
     if results.pose_landmarks:
@@ -48,38 +51,3 @@ cap.release()
 cv2.destroyAllWindows()
 
 
-# FROM THE BLAZEPOSE REPO:
-
-# from BlazeposeDepthaiEdge import BlazeposeDepthai
-# from BlazeposeRenderer import BlazeposeRenderer
-
-# # The argparse stuff has been removed to keep only the important code
-
-# tracker = BlazeposeDepthai(input_src=args.input, 
-#             pd_model=args.pd_m,
-#             lm_model=args.lm_m,
-#             smoothing=not args.no_smoothing,   
-#             xyz=args.xyz,           
-#             crop=args.crop,
-#             internal_fps=args.internal_fps,
-#             internal_frame_height=args.internal_frame_height,
-#             force_detection=args.force_detection,
-#             stats=args.stats,
-#             trace=args.trace)   
-
-# renderer = BlazeposeRenderer(
-#                 pose, 
-#                 show_3d=args.show_3d, 
-#                 output=args.output)
-
-# while True:
-#     # Run blazepose on next frame
-#     frame, body = tracker.next_frame()
-#     if frame is None: break
-#     # Draw 2d skeleton
-#     frame = renderer.draw(frame, body)
-#     key = renderer.waitKey(delay=1)
-#     if key == 27 or key == ord('q'):
-#         break
-# renderer.exit()
-# tracker.exit()

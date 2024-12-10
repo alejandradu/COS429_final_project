23e4bbede341a927fd7ba4c12dcc650c833ab252
Alejandra Duran <ad2002@princeton.edu>
2024-12-10 11:47:17 -0500

    reformat

--- training_pipeline/extract_features.py
+++ training_pipeline/extract_features.py
@@ -5,6 +5,8 @@ import numpy as np
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
 
 # import tensorflow as tf
 # import tensorflow_hub as hub
@@ -125,3 +127,43 @@ def mediapipe_format_landmark(landmarker_result, encoded_label):
     X = X.reshape(1, MP_N_LANDMARKS * 4)
     
     return X, y
+
+# from the Gemini API
+
+def draw_landmarks_on_image(rgb_image, detection_result=None, landmark_coords=None):
+    """Can take in the landmark result object from MP detector or 
+    the coordinates directly as an array (landmark, xyz coords)"""
+    
+    annotated_image = np.copy(rgb_image)
+
+    # can also give the coords directly after processing
+    if landmark_coords is not None:
+        # Draw the pose landmarks.
+        pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        pose_landmarks_proto.landmark.extend([
+          landmark_pb2.NormalizedLandmark(x=landmark_coords[i,0], y=landmark_coords[i,1], z=landmark_coords[i,2]) for i in range(landmark_coords.shape[0])
+        ])
+        solutions.drawing_utils.draw_landmarks(
+          annotated_image,
+          pose_landmarks_proto,
+          solutions.pose.POSE_CONNECTIONS,
+          solutions.drawing_styles.get_default_pose_landmarks_style())
+        return annotated_image
+    
+    pose_landmarks_list = detection_result.pose_landmarks
+
+    # Loop through the detected poses to visualize.
+    for idx in range(len(pose_landmarks_list)):
+        pose_landmarks = pose_landmarks_list[idx] 
+        # Draw the pose landmarks.
+        pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        pose_landmarks_proto.landmark.extend([
+          landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
+        ])
+        solutions.drawing_utils.draw_landmarks(
+          annotated_image,
+          pose_landmarks_proto,
+          solutions.pose.POSE_CONNECTIONS,
+          solutions.drawing_styles.get_default_pose_landmarks_style())
+        
+    return annotated_image
